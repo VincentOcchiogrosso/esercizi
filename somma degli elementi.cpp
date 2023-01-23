c27ddd345febf7b2f9821613029419ddec93822f
ALX-VINCY <123358987+VincentOcchiogrosso@users.noreply.github.com>
2023-01-23 09:31:14 +0100

    Add files via upload

---
+++ somma degli elementi.cpp
@@ -0,0 +1,26 @@
+/*
+	Name: Somma degli Elementi
+	Copyright: Colamonico-chiarulli
+	Author:Vincent Occhiogrosso
+	Date: 18/11/22 13:27
+	Description: Dare in
+output la somma di tutti gli elementi del vettore.
+
+*/
+#include <iostream>
+using namespace std;
+int main (){
+	int i=0;
+	int lungVet [i];
+	int somma=0;
+	
+	for(i=0;i<10;i++)
+	cout<<"inserisci il valore di "<< vettore[i];
+	cin>> vettore[i]
+	somma=somma+vettore[i];
+	
+	cout<<"la somma dei valori dei vettori e\'":<<somma;
+	
+	return 0;
+	
+}
