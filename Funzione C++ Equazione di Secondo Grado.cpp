71b65c5d841ba1979830fd8b736d3f408c3d51a9
ALX-VINCY <123358987+VincentOcchiogrosso@users.noreply.github.com>
2023-02-13 10:30:03 +0100

    Create Funzione C++ Equazione di Secondo Grado.cpp

---
+++ Funzione C++ Equazione di Secondo Grado.cpp
@@ -0,0 +1,38 @@
+/*
+	Name:Funzione C++ Equazione di Secondo Grado
+	Copyright: Colamonico-chiarulli
+	Author:Vincent Occhiogrosso
+	Date: 06/02/23 10:21
+	Description: Scrivi poi un programma che, dati in input i coefficienti dell’equazione, dia in output se l’equazione è
+possibile, e, in caso affermativo, dia in output anche le due radici.
+Commenti sul corso
+*/
+#include <iostream>
+using namespace std;
+
+double CalcolaDelta
+
+int main(){
+	
+	double base;
+	double esponente;
+	
+
+cout<<"inseriscila  base";
+cin>>a;
+cout<<"inserisci l'esponente";
+cin>>b;
+cout<<"la potenza di "<<base>>"elevato alla "<< esponente <<"e'";
+cout<<pow(base,esponente);
+
+
+
+return 0;
+
+}
+double CalcolaDelta (a,b,c){
+double delta;
+delta= pow(base,esponente)-4*a*c;
+delta= sqrt(pow(base,esponente))-4*a*c;+
+return delta;
+}
